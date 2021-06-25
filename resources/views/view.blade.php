35fcc535b093bfe11614ce9800a70336c0c6c79e
dex <dexduque18@bitbucket.org>
2021-06-25 11:24:43 +0800

    commit

---
+++ resources/views/view.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="slot">
+       <!-- Content Wrapper. Contains page content -->
+
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="card card-warning">
+                <div class="card-header">
+                <h3 class="card-title">ACIVITIES</h3>
+                </div>
+        </section>
+
+
+
+
+
+
+      <!-- Main Footer -->
+    </x-slot>
+
+</x-app-layout>
+<!-- ChartJS -->
+
+
+
